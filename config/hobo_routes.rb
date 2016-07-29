0ed2a00e51ca4dc7665811b0542e6a10b9f8cd79
arkaitzz <arkaitzzweb@yahoo.es>
2016-07-29 09:24:12 +0200

    initial

---
+++ config/hobo_routes.rb
@@ -0,0 +1,34 @@
+# This is an auto-generated file: don't edit!
+# You can add your own routes in the config/routes.rb file
+# which will override the routes in this file.
+
+Sgils::Application.routes.draw do
+
+
+  # Resource routes for controller users
+  resources :users, :only => [:index, :edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'signup', :action => 'do_signup'
+      get 'signup'
+    end
+    member do
+      get 'account'
+      put 'activate', :action => 'do_activate'
+      get 'activate'
+      put 'reset_password', :action => 'do_reset_password'
+      get 'reset_password'
+    end
+  end
+
+  # User routes for controller users
+  post 'login(.:format)' => 'users#login', :as => 'user_login_post'
+  get 'login(.:format)' => 'users#login', :as => 'user_login'
+  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
+  get 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+  post 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password_post'
+
+  namespace :concerns do
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sgils::Application.config.secret_key_base = '66044d3e841ce12a2dd052362935b701f57207bca2cac554b9dbeb2d626458ab2fdce5571837357e29ceea99369ba18df12960cf5794a5998939aa7b4b14e936'
