aea8c02fa8278796566d69af6758b568d284cf98
Ibon Castilla Varela <ibon.castilla@gmail.com>
2018-03-25 13:27:00 +0200

    Varios mejoras vistas y ciclo de vida usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   hobo_user_controller
 
-  auto_actions :all, :except => [:index, :new, :create]
+  auto_actions :all, :except => [:index, :new, :create, :edit]
 
   # Normally, users should be created via the user lifecycle, except
   #  for the initial user created via the form on the front screen on
@@ -18,9 +18,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    @user.viewable_by?(current_user) ? hobo_show : redirect_to('/menu') 
+  end
+
   def do_signup
     hobo_create do
-      UserMailer.new_user(this).deliver if valid?
+      if valid?
+        UserMailer.new_user(this).deliver
+        flash[:notice] = t('user.actions.signup_completed')
+        redirect_to home_page
+      end
     end
   end
 
@@ -41,18 +50,22 @@ class UsersController < ApplicationController
   end
 
   def main_menu
+    if current_user.guest?
+      flash[:error] = t('user_forbidden')
+      redirect_to home_page
+    end
     @user = current_user
   end
 
   def role_set
     if !current_user.administrator?
-      flash[:notice] = I18n.t('user_forbidden')
-      redirect_to '/'
+      flash[:notice] = t('user_forbidden')
+      redirect_to home_page
     end
     # Set user
-    @user = User.all
-    @applicants = User.all.applicant
-    @interpreters = User.all.interpreter
+    @user = User.find_each
+    @applicants = User.applicant.find_each
+    @interpreters = User.interpreter.find_each
     hobo_ajax_response if request.xhr?
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
 
   fields do
     name          :string, :required, :unique
-    email_address :email_address, :login => true
+    email_address :email_address, :unique, :login => true
     administrator :boolean, :default => false
-    applicant :boolean, :default => true
-    interpreter :boolean, :default => false
+    interpreter   :boolean, :default => false
+    applicant     :boolean, :default => true
     timestamps
   end
   attr_accessible :name, :email_address, :password, :password_confirmation, :current_password, :addresses
@@ -29,15 +29,31 @@ class User < ActiveRecord::Base
   end
 
   # --- Change user_type --- #
+  def become_administrator_allowed?
+    ['administrator'].include?(acting_user.profile) && !self.administrator?
+  end
+
+  def become_interpreter_allowed?
+    ['administrator'].include?(acting_user.profile) && !self.interpreter?
+  end
+
+  def become_applicant_allowed?
+    ['administrator'].include?(acting_user.profile) && !self.applicant?
+  end
+
+  def become_administrator
+    self.update_attributes(:applicant => false, :interpreter => false, :administrator => true)
+  end
+
   def become_interpreter
-    self.update_attributes(:applicant => false, :interpreter => true)
+    self.update_attributes(:applicant => false, :interpreter => true, :administrator => false)
   end
 
   def become_applicant
-    self.update_attributes(:applicant => true, :interpreter => false)
+    self.update_attributes(:applicant => true, :interpreter => false, :administrator => false)
   end
 
-  # --- Signup lifecycle --- #
+  # --- Lifecycle --- #
   lifecycle do
     state :inactive, :default => true
     state :active
@@ -55,19 +71,31 @@ class User < ActiveRecord::Base
     transition :reset_password, { :active => :active }, :available_to => :key_holder,
                :params => [ :password, :password_confirmation ]
 
-    transition :activate, { :inactive => :active }, :available_to => :key_holder
+    transition :be_administrator, { :active => :active }, :available_to => :all, :if => :become_administrator_allowed? do
+      self.become_applicant
+    end
 
-    transition :be_interpreter, { :active => :active }, :available_to => :all do
+    transition :be_interpreter, { :active => :active }, :available_to => :all, :if => :become_interpreter_allowed? do
       self.become_interpreter
     end
 
-    transition :be_applicant, { :active => :active }, :available_to => :all do
+    transition :be_applicant, { :active => :active }, :available_to => :all, :if => :become_applicant_allowed? do
       self.become_applicant
     end
+
+    transition :mark_as_inactive, { :active => :inactive }, :available_to => 'User.administrator'
+    transition :mark_as_active, { :inactive => :active }, :available_to => 'User.administrator'
   end
 
+  # --- Aux. methods --- #
   def signed_up?
     state == 'active'
+  end
+
+  def profile
+    return 'administrator' if administrator?
+    return 'interpreter' if interpreter?
+    return 'applicant' if applicant?
   end
 
   # --- Permissions --- #
@@ -77,9 +105,9 @@ class User < ActiveRecord::Base
 
   def update_permitted?
     acting_user.administrator? ||
-      (acting_user == self && only_changed?(:email_address, :crypted_password,
+      (acting_user == self && only_changed?(:name, :email_address, :crypted_password,
                                             :current_password, :password, :password_confirmation))
-    # Note: crypted_password has attr_protected so although it is permitted to change, it cannot be changed
+    # NOTE: crypted_password has attr_protected so although it is permitted to change, it cannot be changed
     # directly from a form submission.
   end
 
@@ -88,6 +116,6 @@ class User < ActiveRecord::Base
   end
 
   def view_permitted?(field)
-    true
+    acting_user == self || acting_user.administrator?
   end
 end
